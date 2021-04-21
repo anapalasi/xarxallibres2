af951a52043d0a256dc9125f2efcc0942be9fc97
Ana Isabel Palasi Lallana <anapalasi@ieshonorigarcia.net>
2021-04-21 13:19:30 +0200

    Pruebas

--- admin/config.php
+++ admin/config.php
@@ -1,14 +1,13 @@
 <?php
 
-define('RUTA', 'http://10.2.0.254/xarxallibres2/');
-
+define('RUTA', 'http://localhost/xarxallibres2/');
+ 
 // nombre de base de datos
 $bd_config = array(
     'db_name' => 'xarxallibres',
     'user' => 'root',
     'pass' => 'root12002661'    
 );
-
 
 ?>
 
--- login.php
+++ login.php
@@ -1,33 +1,7 @@
-<?php session_start();
+<?php 
 
 	require 'admin/config.php';
 	require 'functions.php';
-
-	$errores = '';
-
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	  $usuario = $_POST['usuario'];
-	  $password = $_POST['password'];
-	  $password = hash('sha512', $password);
-
-  	$conexion = conexion($bd_config);
-	$statement = $conexion->prepare('SELECT * FROM Profesor WHERE dni  = :usuario AND contrasenya = :password');
-  	$statement->execute(
-	    array(
-        	':usuario' => $usuario,
-        	':password' => $password    
-    	)    
-  	);
-  $resultado = $statement->fetch();
-
-  if ($resultado !== false) {
-    $_SESSION['usuario'] = $usuario;
-    header('Location: '.RUTA.'index.php');
-  } else {
-    $errores .= '<li class="error">Tu usuario y/o contraseña son incorrectos</li>';
-  }
-
-}
 	require 'views/login.view.php';
 ?>
 
