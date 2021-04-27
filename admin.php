3c77ac55fa6bf46b1b1354562243bd7072149e42
Anabel <anapalasi@gmail.com>
2021-04-27 09:43:58 +0200

    Creacion admin.php

---
+++ admin.php
@@ -0,0 +1,22 @@
+<?php
+  // Abrimos la sesion
+  session_start();
+  
+  require 'admin/config.php'
+  require 'functions.php'
+  
+  // Comprobamos la sesión
+  if (!isset($_SESSION['usuario'])){
+    header('Location:login.php');
+  }
+  
+  $conexion = conexion($bd_config);
+  $admin= iniciarSession('Profesor',$conexion);
+  
+  if ($admin == 'administrador'){
+    require 'views/admin.php'
+  }
+  else {
+    header('Location:index.php');
+  }
+?>
