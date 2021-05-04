f95c1d0c893f359ca400bb269c95221f8d43383f
Ana Isabel Palasi Lallana <anapalasi@ieshonorigarcia.net>
2021-05-04 12:00:41 +0200

    Valoracio 2

--- views/valoraGrup.view.php
+++ views/valoraGrup.view.php
@@ -14,15 +14,19 @@
   <p align="center"> <img src="img/xarxa_llibres-300x150.png" alt="Logo Xarxa Llibres"></p><br>
   <h2 class="texto">Llistat d'alumnes de <?php echo utf8_encode($grup['descripcion']); ?> </h2>
   <br>
+  <form action="actualizaPuntuacion.php" method="post">
+  <table border="1" align="center">
+  <tr><th> Nombre </th> </th> Apellidos </th><
   <?php
 	foreach ($llibres as $llibre){
-		echo utf8_encode($llibre["nombre"]). " " . utf8_encode($llibre["apellido1"]). " " . utf8_encode($llibre["apellido2"]). " " . $llibre["volumen"] . " " . $llibre["puntos"];
-//		echo $llibre["nombre"]. " " . $llibre["apellido1"]. " " . $llibre["apellido2"];
+	
+		echo utf8_encode($llibre["nombre"]) . " " . utf8_encode($llibre["apellido1"]). " " . utf8_encode($llibre["apellido2"]). " ". $llibre["ejemplar"] . " " . $llibre["volumen"]. " ". $llibre["puntos"];
 		echo "<br>";
 
 	}
 ?>
-
+ </table>
+ </form>
   <a href="close.php">Cerrar Sesion</a>
 </body>
 </html>
