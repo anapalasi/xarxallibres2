2f0c07460cd5f9d463f572dbe4bcd5d03823f809
Anabel Palasí <anapalasi@ieshonorigarcia.net>
2021-05-14 08:33:38 +0200

    Falta inserir/esborrar checkbox

--- actualizaPuntuacion.php
+++ actualizaPuntuacion.php
@@ -20,51 +20,34 @@
 
 	// Obtenim totes les observacions
 	$llistat_observacions = obteObservacions($conexion);
-	
-			
-	
+				
+	// Observacions formulari
+	$observacions_formulari = array();
+
+
+
 	for($i=0;$i<count($_POST['ejemplar']);$i++){
 		$sql = "update Ejemplar set puntos  = '".$_POST['estat'][$i]." ' where id_ejemplar = '".$_POST['ejemplar'][$i]."'";
 		$resultat = executaSentencia($conexion,$sql);
 		$indice=1;
-		// Recorrem totes les possibles observacions
-		var_dump($_POST['observacions'][$i]);
-		foreach ($variable as $valor)
-		{	
-			// Comprovarem si el valor se troba en la base de dades. En cas de que siga distint de cadena
-			// buida esta en la base de dades.
-			$esta=observacioRegistrada($conexion, $_POST['ejemplar'][$i], $indice);
-			if ($esta != ""){
-				echo $valor;
-				echo "Valor base de dades: " . $esta . "-> ". !empty($_POST[$valor][$i]). " " . $i . "<br>";
-			}
-			
-			// Si estava marcat al formulari
-		/*	$esta_formulari = isset($POST[$valor][$i]);
-			if ($valor == $_POST[$valor][$i])
-				echo "Eureka";
-			echo $_POST['ejemplar'][$i]. " ". $valor. " Esta:". $esta_formulari . "<br>";	
-			if (!empty($_POST[$valor][$i])){
-				echo "Entra <br>"; 
-				// Si está activat hem de comprobar si ja estava inserit
-				if (! $esta){
-					echo $valor . " " .$esta.  "No estava";
-				}					
-
-			}
-			else
-			{
-				// Comprovem si estava activat per desactivar-ho
-				if ($esta){
-					echo $esta . "Cal esborrar-lo";
-				}
-				
-			}*/
-			$indice++;
-		}
-		
+		// Si la observacio es diferent de NULL aleshores s'afig a l'array corresponent
+		if ($_POST['observacions'][$i])
+			array_push($observacions_formulari, $_POST['observacions'][$i]);
 	}
     
+    /* Comprovem si les observacions guardades es mantenen i si no les esborrem */
+	foreach ($llistat_observacions as $guardada){
+		// Si no está les esborrarem
+		if (!in_array($guardada, $observacions_formulari))
+			echo $guardada . "s'ha esborrat";
+	}
+
+	/* Comprovem si les observacions del formulari estaven ja guardades i si no les inserirem */
+	foreach ($observacions_formulari as $nova_obs){
+		if (! in_array($nova_obs, $llistat_observacions))
+			echo $nova_obs . "s'ha inserit";
+	}
+
     $usuario= iniciarSession('Profesor', $conexion); // Obtenemos el rol del usuario
 	
 	
